6577b15a37428f27a959cd4d88b5a04c8f20f2a4
zyf <zhaoyafei0210@gmail.com>
2017-09-08 05:07:07 +0800

    add mkdir(default_save_dir)

--- upload.py
+++ upload.py
@@ -1,7 +1,21 @@
-import tornado.httpserver, tornado.ioloop, tornado.options, tornado.web, os.path, random, string
+import os.path as osp
+import os
+import random
+import string
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+
 from tornado.options import define, options
 
-define("port", default=8888, help="run on the given port", type=int)
+define("port", default=8899, help="run on the given port", type=int)
+
+default_save_dir = './uploads'
+
+print "Default save dir: " + default_save_dir
+if not osp.exists(default_save_dir):
+    os.makedirs(default_save_dir)
 
 class Application(tornado.web.Application):
     def __init__(self):
@@ -10,26 +24,41 @@ class Application(tornado.web.Application):
             (r"/upload", UploadHandler)
         ]
         tornado.web.Application.__init__(self, handlers)
-        
+
+
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("upload_form.html")
-        
+
+
 class UploadHandler(tornado.web.RequestHandler):
     def post(self):
         file1 = self.request.files['file1'][0]
         original_fname = file1['filename']
-        extension = os.path.splitext(original_fname)[1]
-        fname = ''.join(random.choice(string.ascii_lowercase + string.digits) for x in range(6))
-        final_filename= fname+extension
-        output_file = open("uploads/" + final_filename, 'w')
+        print "Received file: " + original_fname
+
+        save_fname = osp.join(default_save_dir, original_fname)
+
+        if osp.exists(save_fname):
+            splits = osp.splitext(save_fname)
+            fname = ''.join(random.choice(
+                string.ascii_lowercase + string.digits) for x in range(6))
+            save_fname = splits[0] + '_' + fname + splits[1]
+
+        output_file = open(save_fname, 'wb')
         output_file.write(file1['body'])
-        self.finish("file" + final_filename + " is uploaded")
-        
+        output_file.close()
+
+        print "file %s is uploaded, saved as %s" % (original_fname, save_fname)
+        self.finish("file %s is uploaded, saved as %s" %
+                    (original_fname, save_fname))
+
+
 def main():
     http_server = tornado.httpserver.HTTPServer(Application())
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
-    
+
+
 if __name__ == "__main__":
     main()
